f1a46366b61ded8d0028edac1a0ac08fa400cdaf
Subhan Ul Haque <iamhaque@gmail.com>
2022-07-17 22:22:56 +0530

    API utils fixed

--- utils/API.js
+++ utils/API.js
@@ -1,37 +1,49 @@
 export const getLeaderboardData = async (hostname) => {
-  const URL = hostname ? hostname + "/api/leaderboard" : "/api/leaderboard";
+  try {
+    const URL = hostname ? hostname + "/api/leaderboard" : "/api/leaderboard";
 
-  const requestOptions = {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  };
-  const response = await fetch(URL, requestOptions);
-  const data = await response.json();
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    const response = await fetch(URL, requestOptions);
+    const data = await response.json();
 
-  if (data && data.status && data.status === "success") {
-    return [...data.leaderboard];
+    if (data && data.status && data.status === "success") {
+      return [...data.leaderboard];
+    }
+
+    return [];
+  } catch (e) {
+    return [];
   }
-
-  return [];
 };
 
 export const updateLeaderboard = async (username, score) => {
-  const requestOptions = {
-    method: "POST",
-    body: JSON.stringify({ username, score }),
-    headers: { "Content-Type": "application/json" },
-  };
+  try {
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify({ username, score }),
+      headers: { "Content-Type": "application/json" },
+    };
 
-  await fetch("/api/updateHighscore", requestOptions);
+    await fetch("/api/updateHighscore", requestOptions);
+  } catch (e) {
+    return;
+  }
 };
 
 export const createNewUser = async (username) => {
-  const requestOptions = {
-    method: "POST",
-    body: JSON.stringify({ username }),
-    headers: { "Content-Type": "application/json" },
-  };
+  try {
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify({ username }),
+      headers: { "Content-Type": "application/json" },
+    };
 
-  const response = await fetch("/api/createUser", requestOptions);
-  return await response.json();
+    const response = await fetch("/api/createUser", requestOptions);
+    return await response.json();
+  } catch (e) {
+    return { status: "failed", message: "Unknown error occurred" };
+  }
 };
